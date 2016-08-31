b96d8c654697247e6c5bace6873fb4d7a3c38b6e
jayesh <jayesh@amuratech.com>
2016-08-31 12:46:11 +0530

    added actions in post controller look at rake routes the routes containing prefix url from them views should be generated, views for posts action controller generated

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,4 +4,27 @@ class PostsController < ApplicationController
 
   end
 
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def show
+
+  end
+
+  def destroy
+
+  end
 end
