708a36d3a6f410982604790e7f9db5deb32218b6
jayesh <jayesh@amuratech.com>
2016-09-01 13:17:55 +0530

    scaffolding categories

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
 
   resources :posts  #posts is the controller
   resources :categories #resouces declared to use in the url
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160831084653) do
+ActiveRecord::Schema.define(version: 20160901065058) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
